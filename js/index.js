178676bef54c09294afc07762a0e1d98e2ce34d7
Diego R <46231316+DiegoRoney@users.noreply.github.com>
2022-10-19 10:46:30 -0300

    btn darkmode

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 const $html = document.querySelector('html')
-const $checkbox = document.querySelector('#switch')
+const $checkbox = document.querySelector('#switch-shadow')
 
 $checkbox.addEventListener('change', function(){
     $html.classList.toggle('dark-mode')
