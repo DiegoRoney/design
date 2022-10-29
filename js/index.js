76c1031eda99e194cf96f2229ea09803d414c50b
Diego R <diego-roney@outlook.com>
2022-10-29 13:52:02 -0300

    new btn

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 const $html = document.querySelector('html')
-const $checkbox = document.querySelector('#switch-shadow')
+const $checkbox = document.querySelector('#dark')
 
 $checkbox.addEventListener('change', function(){
     $html.classList.toggle('dark-mode')
